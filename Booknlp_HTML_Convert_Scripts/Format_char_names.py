dd3e79f9f74b2d8992d9e538e46747f84799c7e1
Drew <drew@MT-MBP-1895.localdomain>
2023-03-05 15:14:12 -0500

    added Format_char_names.py adn updated the Run_Order file accordingly

---
+++ Booknlp_HTML_Convert_Scripts/Format_char_names.py
@@ -0,0 +1,16 @@
+#this will be used to make it so that theres no repeating names in the output file
+#I also removes any "[" or "]" from the input file being the 1char file
+with open("/content/1char_names.txt", "r") as input_file, open("2char_names.txt", "w") as output_file:
+    lines_seen = set()
+    for line in input_file:
+        # Remove "[" and "]" symbols from line
+        line = line.replace("[", "").replace("]", "")
+        # Remove numbers from line
+        line = ''.join([i for i in line if not i.isdigit()])
+        # Remove leading and trailing whitespace
+        line = line.strip()
+        # Check if line is a duplicate
+        if line not in lines_seen:
+            # If line is not a duplicate, add it to the set of seen lines and write it to output file
+            lines_seen.add(line)
+            output_file.write(line + "\n")
